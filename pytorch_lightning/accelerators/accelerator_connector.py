a0a6adaadfa94e248ea63f3825666aef7cda21e2
William Falcon <waf2107@columbia.edu>
2020-09-29 22:59:18 -0400

    ref: decoupled ddp spawn

--- pytorch_lightning/accelerators/accelerator_connector.py
+++ pytorch_lightning/accelerators/accelerator_connector.py
@@ -76,9 +76,6 @@ class AcceleratorConnector:
 
         # override with environment flag
         gpus = os.environ.get('PL_TRAINER_GPUS', gpus)
-        print('-'*100)
-        print('env GPUs', gpus)
-        print('-'*100)
         self.trainer.gpus = gpus
 
         # for gpus allow int, string and gpu list
